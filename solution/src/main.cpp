f0a82bcdd4a784e20aeda55bfa6fb54bd6c21070
Rémi PIGNOL <remi.pignol@epitech.eu>
2019-02-28 20:13:47 +0100

    modification du main en concéquence du commit précèdent

--- solution/src/main.cpp
+++ solution/src/main.cpp
@@ -6,11 +6,6 @@
 */
 
 #include <iostream>
-/*#include "dlloader/ModulesManager.hpp"
-#include "server/Server.hpp"
-#include "server/Request.hpp"
-#include "server/ConfigManager.hpp"
-*/
 #include "server/ShellServer.hpp"
 
 int main(int ac, char **av)
@@ -20,21 +15,6 @@ int main(int ac, char **av)
 		return EXIT_FAILURE;
 	}
 	try {
-		// get config
-/*		zia::server::ConfigManager configManager(av[1]);
-		auto config = std::move(configManager.getConfig());
-
-		zia::dlloader::ModulesManager modulesManager(config);
-		zia::server::Server server(config);
-
-		auto handleRequest = zia::server::Callback(std::bind(
-			[](dems::StageManager &stageManager, zia::server::SocketPtr socket)-> void{
-				// lambda who handle a request
-				zia::server::Request request(stageManager, std::move(socket));
-				request.handleRequest();
-			}, modulesManager.getStageManager(), std::placeholders::_1));
-
-		server.run(handleRequest);*/
 		zia::server::ShellServer shell(av[1]);
 		shell.run();
 	} catch (const std::exception &e) {
