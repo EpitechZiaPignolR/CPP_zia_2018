f1797f80504fb92f134c54d297b9a4218cef3789
benoit.jallet <benoit.jallet@epitech.eu>
2019-03-10 16:27:42 +0100

    add Gzip module

--- module/GZipper/src/GZipper.cpp
+++ module/GZipper/src/GZipper.cpp
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2021
 ** zia
 ** File description:
-** ModuleDeflateZipper.cpp
+** ModuleGZipper.cpp
 */
 
 #include <cstdio>
@@ -11,25 +11,25 @@
 #include <string>
 #include <boost/iostreams/filtering_streambuf.hpp>
 #include <boost/iostreams/copy.hpp>
-#include <boost/iostreams/filter/zlib.hpp>
-#include "ModuleDeflateZipper.hpp"
+#include <boost/iostreams/filter/gzip.hpp>
+#include "ModuleGZipper.hpp"
 
-dems::CodeStatus ModuleDeflateZipper(dems::Context &context)
+dems::CodeStatus ModuleGZipper(dems::Context &context)
 {
     auto &request = context.request;
     auto &response = context.response;
     auto &accept_encoding = request.headers->getHeader("Accept-Encoding");
-    if (accept_encoding == "" || accept_encoding.find("deflate") == accept_encoding.npos)
+    if (accept_encoding == "" || accept_encoding.find("gzip") == accept_encoding.npos)
         return dems::CodeStatus::DECLINED;
     else
     {
-        response.headers->setHeader("content-encoding", "deflate");
+        response.headers->setHeader("content-encoding", "gzip");
 
         std::stringstream compressed;
         std::stringstream new_body;
         new_body << response.body;
         boost::iostreams::filtering_streambuf<boost::iostreams::input> out;
-        out.push(boost::iostreams::zlib_compressor());
+        out.push(boost::iostreams::gzip_compressor());
         out.push(new_body);
         boost::iostreams::copy(out, compressed);
         response.body = compressed.str();
@@ -40,7 +40,7 @@ dems::CodeStatus ModuleDeflateZipper(dems::Context &context)
 extern "C" {
     std::string registerHooks(dems::StageManager &manager)
     {
-        manager.request().hookToEnd(50000, "DeflateZipper", ModuleDeflateZipper);
-        return "DeflateZipper";
+        manager.request().hookToEnd(50000, "GZipper", ModuleGZipper);
+        return "GZipper";
     }
 }
